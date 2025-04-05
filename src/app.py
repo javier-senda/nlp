8d8ffc2876d413b1d03827f921bb38f66d929ab5
javier-senda <javiersendarrubias@hotmail.com>
2025-04-05 15:39:40 +0000

    entrega final

--- src/app.py
+++ src/app.py
@@ -1,4 +1,94 @@
-from utils import db_connect
-engine = db_connect()
+import pandas as pd
+import re
+from nltk import download
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+import pickle
+import json
 
-# your code here
+# Importar el dataset
+total_data = pd.read_csv("https://raw.githubusercontent.com/4GeeksAcademy/NLP-project-tutorial/main/url_spam.csv")
+
+total_data.to_csv("../data/raw/total_data.csv", index = False)
+
+# Procesamiento de datos
+total_data["is_spam"] = total_data["is_spam"].apply(lambda x: 1 if x else 0).astype(int)
+
+total_data = total_data.drop_duplicates()
+total_data = total_data.reset_index(inplace = False, drop = True)
+
+def preprocess_text(text):
+    text=text.lower()
+
+    text = re.sub(r'[^a-z ]', " ", text)
+    
+    # Eliminar espacios en blanco
+    text = re.sub(r'\s+[a-zA-Z]\s+', " ", text)
+    text = re.sub(r'\^[a-zA-Z]\s+', " ", text)
+
+    # Reducir espacios en blanco múltiples a uno único
+    text = re.sub(r'\s+', " ", text.lower())
+
+    # Eliminar tags
+    text = re.sub("&lt;/?.*?&gt;"," &lt;&gt; ", text)
+
+    return text.split()
+
+total_data["url"] = total_data["url"].apply(preprocess_text)
+
+download("wordnet")
+lemmatizer = WordNetLemmatizer()
+
+download("stopwords")
+stop_words = stopwords.words("english")
+
+def lemmatize_text(words, lemmatizer = lemmatizer):
+    tokens = [lemmatizer.lemmatize(word) for word in words]
+    tokens = [word for word in tokens if word not in stop_words]
+    tokens = [word for word in tokens if len(word) > 3]
+    return tokens
+
+total_data["url"] = total_data["url"].apply(lemmatize_text)
+
+wordcloud = WordCloud(width = 800, height = 800, background_color = "black", max_words = 1000, min_font_size = 20, random_state = 42)\
+    .generate(str(total_data["url"]))
+
+fig = plt.figure(figsize = (8, 8), facecolor = None)
+plt.imshow(wordcloud)
+plt.axis("off")
+plt.show()
+
+tokens_list = total_data["url"]
+tokens_list = [" ".join(tokens) for tokens in tokens_list]
+
+vectorizer = TfidfVectorizer(max_features = 5000, max_df = 0.8, min_df = 5)
+X = vectorizer.fit_transform(tokens_list).toarray()
+y = total_data["is_spam"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+
+X_train = pd.DataFrame(X_train)
+X_test = pd.DataFrame(X_test)
+y_train = pd.DataFrame(y_train)
+y_test = pd.DataFrame(y_test)
+
+X_train.to_excel("../data/processed/X_train.xlsx", index = False)
+X_test.to_excel("../data/processed/X_test.xlsx", index = False)
+y_train.to_excel("../data/processed/y_train.xlsx", index = False)
+y_test.to_excel("../data/processed/y_test.xlsx", index = False)
+
+model = SVC(kernel = "linear", random_state = 42)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+accuracy_score(y_test, y_pred)
+
+from pickle import dump
+
+dump(model, open("../models/svm_classifier_linear_42.sav", "wb"))
